bf5f1f186cec98aad436d3351612c4e6c6ac7a3d
lin <13889305019@139.com>
2020-12-12 18:45:20 +0800

    router config

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Labels.vue</h1>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Labels"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Statistics.vue</h1>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Statistics"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
