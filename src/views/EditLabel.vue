9a0b7130697e438f9dd11e5b24c96ecb0041a400
lin <13889305019@139.com>
2020-12-20 21:34:13 +0800

    add EditLabel.vue of view

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <h1>修改标签</h1>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+import tagsModel from '@/model/tagsListModel';
+@Component
+export default class EditLabel extends Vue{
+  created(){
+    const id = this.$route.params.id;
+    console.log(id);
+    if(id){
+      tagsModel.fetch();
+      console.log(tagsModel.data);
+      const item = tagsModel.data.filter(i=> i.id===id)[0];
+      if(item){
+        console.log(item);
+      }else {
+        this.$router.push('/404')
+      }
+
+    }
+
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
