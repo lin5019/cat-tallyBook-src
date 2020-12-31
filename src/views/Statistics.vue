ee9ea0d566b92f521c7bf7d1460c1ec69a0e1f3b
lin <13889305019@139.com>
2020-12-31 11:26:58 +0800

    test statistics scss style

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,10 +3,15 @@
     <Tabs class-prefix="type" :array.sync="typeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :array.sync="intervalList" :value.sync="interval"/>
     <ul>
-      <li v-for="(items,index) in result" :key="index" >
-        {{items.title}}
+      <li v-for="(items,index) in result" :key="index">
+        <h3 class="title">{{ items.title }}</h3>
         <ul>
-          <li v-for="i in items.list" :key="i.createAt">{{i.amount}}</li>
+          <li class="record" v-for="i in items.list" :key="i.createAt">
+            <span>{{ TagsName(i.tags) || '无' }}</span>
+            <span class="notes">{{ i.notes }}你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好</span>
+            <span>¥{{i.amount }}</span>
+          </li>
+
         </ul>
       </li>
     </ul>
@@ -30,26 +35,58 @@ export default class Statistics extends Vue {
   typeList = typeList;
   type = typeList[0].value;
 
-  get recordList(){
-    return (this.$store.state as RootState).recordList
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
   }
 
-  get result(){
+  get result() {
     const {recordList} = this;
-    const hashTable: HashTable = {}
-    for (let i = 0; i <recordList.length ; i++) {
-      const [date,time]=recordList[i].createAt.split('T')
+    const hashTable: HashTable = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createAt.split('T');
       //第一次是一个空数组,给一个初始值.
-      hashTable[date] = hashTable[date] || {title: date,list: []}
-      hashTable[date].list.push(recordList[i])
+      hashTable[date] = hashTable[date] || {title: date, list: []};
+      hashTable[date].list.push(recordList[i]);
     }
-    return hashTable
+    return hashTable;
+  }
+
+  get tagList(){
+    return (this.$store.state as RootState).tagList;
+  }
+
+ TagsName(ids){
+    const tagsName=[]
+    for (let i = 0; i <ids.length ; i++) {
+      const id=ids[i]
+      tagsName.push(this.tagList.filter(i=>i.id===id)[0].name)
+    }
+   return  tagsName.join('-')
   }
 }
 
 </script>
 
 <style lang="scss" scoped>
+%item {
+  line-height: 24px;
+  padding: 8px 16px;
+}
+.title {
+  @extend %item;
+}
+.record {
+  @extend %item;
+  background: #e5e5e5;
+  //border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  & .notes {
+  margin: 0 16px;
+  }
+}
+
 ::v-deep {
   .type-tabs-item {
     background: #fff;
